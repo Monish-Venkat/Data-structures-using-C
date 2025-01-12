adc3253b1314c9cc6ab8d8b9c1bf1211c25f9c84
Monish-Venkat <154602143+Monish-Venkat@users.noreply.github.com>
2025-01-12 21:18:20 +0530

    Add files via upload

---
+++ ads programs/5.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<ctype.h>
+#define max 5
+int stack[max],i,top=-1;
+void push(int x){
+    stack[++top]=x;
+}
+int pop(){
+    return stack[top--];
+}
+int evaluatePostfix(char *exp){
+    for(i=0;exp[i]!='\0';i++){
+        if(isdigit(exp[i])){
+            push(exp[i] - '0');
+        }else{
+        if(top<0) return -1;
+        int op2=pop(),op1=pop();
+        switch(exp[i]){
+            case '+':push(op1+op2);break;
+            case '-':push(op1-op2);break;
+            case '*':push(op1*op2);break;
+            case '/':push(op1/op2);break;
+            default: return -1;
+        }
+    }
+}
+return (top==0)?pop():-1;
+}
+int main(){
+    char exp[max];
+    printf("Enter postfix expression: ");
+    scanf("%s",exp);
+    int result=evaluatePostfix(exp);
+    printf(result==-1?"Invalid expression":"Result : %d",result);
+    return 0;
+}
